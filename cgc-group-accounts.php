5b62d980f7e7d82e57e23ff9fa7a2e77c0341b22
Pippin Williamson <pippin@pippinsplugins.com>
2014-08-27 11:28:57 -0500

    Add first plugin file

---
+++ cgc-group-accounts.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: CG Cookie - Group Accounts
+Plugin URL: https://github.com/CGCookie/cgc-group-accounts
+Description: Provides the database and API for group accounts on the CG Cookie education network
+Version: 0.1
+Author: Pippin Williamson
+Author URI: http://cgcookie.com
+*/
